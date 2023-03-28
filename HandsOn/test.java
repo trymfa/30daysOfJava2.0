a4ce750d295440035104e1e316b45f4a8f75e97e
trymfa <trymfalkum@gmail.com>
2023-03-28 13:01:39 +0200

    testforgit

---
+++ HandsOn/test.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+  public static void main(String[] args) {
+      
+  }
+}
